b2ce177622cfae687239e6f12e151ec56e3a1a42
@Hqs2702 <quocsang05.uit@gmail.com>
2025-04-14 12:42:50 +0700

    create HomeActivity

---
+++ app/src/main/java/com/example/metro_app/Activity/AdHomeActivity.java
@@ -0,0 +1,7 @@
+package com.example.metro_app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdHomeActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/example/metro_app/AdHomeActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.metro_app;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class AdHomeActivity extends AppCompatActivity {
-
-}
