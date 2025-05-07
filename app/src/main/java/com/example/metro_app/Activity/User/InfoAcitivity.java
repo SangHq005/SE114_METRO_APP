00d886f79a61e4306d7d4bf21841ede5e132d181
Quốc Sang <149033042+SangHq005@users.noreply.github.com>
2025-05-07 10:24:42 +0700

    fix: fix all layout user

--- app/src/main/java/com/example/metro_app/Activity/User/InfoAcitivity.java
+++ app/src/main/java/com/example/metro_app/Activity/User/InfoAcitivity.java
@@ -1,6 +1,10 @@
 package com.example.metro_app.Activity.User;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +17,19 @@ import com.example.metro_app.databinding.ActivityInfoAcitivityBinding;
 
 public class InfoAcitivity extends AppCompatActivity {
     ActivityInfoAcitivityBinding binding;
-
+    ImageView backbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         binding = ActivityInfoAcitivityBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(binding.getRoot());
-
-
+        backbtn = findViewById(R.id.backBtn);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(InfoAcitivity.this, "Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
