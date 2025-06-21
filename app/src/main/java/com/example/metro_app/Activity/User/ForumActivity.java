e8dffa0029ce3775691907a1df7e1cd9ade335d2
Quốc Sang <149033042+SangHq005@users.noreply.github.com>
2025-06-21 10:23:07 +0700

    Add: add layout ForumMetro

---
+++ app/src/main/java/com/example/metro_app/Activity/User/ForumActivity.java
@@ -0,0 +1,14 @@
+package com.example.metro_app.Activity.User;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.metro_app.R;
+
+public class ForumActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum);
+    }
+}
